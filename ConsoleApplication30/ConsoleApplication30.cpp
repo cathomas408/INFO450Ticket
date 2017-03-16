01c4eadd86b2d52edefaa3bd22d580c6cb5b108b
cathomas408 <cathomas408@gmail.com>
2017-03-15 22:21:44 -0400

    Final Submission

---
+++ ConsoleApplication30/ConsoleApplication30.cpp
@@ -0,0 +1,139 @@
+// ConsoleApplication30.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class SystemTicket
+{
+protected:
+	int ID;
+	int ticket;
+	int getPriority;
+	string FullName, getType, getDescription, getImpact, Ticket_ID, TicketStatus, Priority;
+public:	
+
+	SystemTicket() {};
+	SystemTicket(string t, string a, string c, string i)
+	{
+		FullName = t; getType = a;
+		getImpact = c; getDescription = i;
+
+	}
+	virtual void Display() = 0;
+	virtual void CaptureData() {};
+	virtual string SetStatus(string a) = 0;
+	virtual string GetInformation(string a) = 0;
+};
+
+class Ticket : public SystemTicket
+{
+public:
+	Ticket() {};
+	Ticket(string t, string a, string c, string i, string e) : SystemTicket(t, a, c, i)
+	{
+	}
+	void Display();
+	void CaptureData();
+	string SetStatus(string a);
+	string GetInformation(string a);
+};
+
+void Ticket::CaptureData()
+{
+	
+	cout << "UserName: ";
+	getline(cin, FullName);
+	cout << endl << "What type of issue is this? (S)erver, (A)pplication, a(C)cess" << endl;
+	getline(cin, getType);
+	cout << endl << "How many users does this impact?";
+	getline(cin, getImpact);
+	cout << "Enter a description of this issue: " << endl;
+	getline(cin, getDescription);
+	cout << endl << "How many users does this impact" << endl;
+	cin >> getPriority;
+		if ((getPriority > 25) && (getPriority < 50)) {
+			Priority = "Medium";
+		}
+		else if ((getPriority >= 50) && (getPriority <= 100)) {
+			Priority = "High";
+		}
+		else { Priority = "Low"; }
+	
+}
+string Ticket::SetStatus(string a)
+{
+	TicketStatus = a;
+	return TicketStatus;
+}
+string Ticket::GetInformation(string a)
+{
+	TicketStatus = a;
+	return TicketStatus;
+}
+
+void Ticket::Display()
+{
+	cout << "Customers Full Name: " << FullName << endl;
+	cout << "Service Issue: " << getType << endl;
+	cout << "How many users impacted: " << getImpact << endl;
+	cout << "Description: " << endl;
+	cout << getDescription << endl << endl;
+	cout << "Ticket Status: " << Priority << endl;
+
+}
+
+int main()
+{
+	SystemTicket **tlist;
+	int count=100;
+	char ptype;
+	bool isOver = false;
+
+	
+
+	tlist = new SystemTicket*[count];
+
+	for (int i = 0; i < count; i++)
+	{
+		
+			tlist[i] = new Ticket();
+			
+
+		tlist[i]->CaptureData();
+		tlist[i]->SetStatus("Open");
+		tlist[0]->SetStatus("Closed");
+		cout << endl << "Do you wish to enter another ticket? (Y)es / (N)" << endl;
+		cin >> ptype;
+			if (toupper(ptype) == 'Y')
+			{ continue;	}
+			else if (toupper(ptype) == 'N')
+			{
+				count = i + 1;
+				isOver = true;
+				break;
+			}
+	}
+	if (isOver == true) {
+		//Extra credit
+	}
+
+
+	for (int i = 0; i < count; i++)
+	{
+		cout << "Ticket Identification: " << i+1000 << endl;
+		tlist[i]->Display();
+		cout << endl;
+	}
+	
+	for (int i = 0; i < count; i++)
+	{
+		delete tlist[i];
+	}
+	delete tlist;
+
+	return 0;
+}
